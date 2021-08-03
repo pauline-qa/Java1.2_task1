a009f2369701aef9b6539c333ea382bbf7f56671
Polina Novikova <novikovapauline@gmail.com>
2021-08-03 11:54:56 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialAmount = 500_000;
+        int transferMoney = hi;
+        int total = initialAmount + transferMoney;
+        System.out.println(total);
+    }
+}
